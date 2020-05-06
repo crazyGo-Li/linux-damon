dcaecc969190b8b55334aa23e7617e90e380506c
crazyGo-Li <48678562+crazyGo-Li@users.noreply.github.com>
2020-05-06 21:28:05 +0800

    add mmap

---
+++ 08-m_map.c
@@ -0,0 +1,41 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <stdio.h>
+
+#define FILELENGTH 80
+
+int main(void)
+{
+	int fd = -1;
+	char buf[] = "quick brown fox jumps over the lazy dog";
+	char *ptr = NULL;
+
+	fd = open("mmap.txt", O_RDWR|O_CREAT|O_TRUNC|S_IRWXU);
+	if(fd == -1)
+	{
+		printf("open mmap.txt fail\n");
+		return -1;
+	}
+
+	lseek(fd, FILELENGTH-1, SEEK_SET);
+
+	write(fd, "a", 1);
+
+	ptr = (char*)mmap(NULL, FILELENGTH, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	if((char*)-1 == ptr)
+	{
+		printf("mmap fail\n");
+		close(fd);
+		return -1;
+	}
+
+	memcpy(ptr+16, buf, strlen(buf));
+	munmap(ptr, FILELENGTH);
+	close(fd);
+	
+	return 0;
+}
