886b20ebfdcd2029a94665350c5d874d9773024c
crazyGo-Li <48678562+crazyGo-Li@users.noreply.github.com>
2020-05-07 21:22:27 +0800

    repaire fcntl bug

--- 10-fcntl01.c
+++ 10-fcntl01.c
@@ -11,23 +11,84 @@
 int main(void)
 {
 	int flags = -1;
+	int accmode = -1;
 	char buf[] = "FCNTL";
-	int fd = open("test.txt", O_RDWR|O_CREAT);
+	int fd = open("test.txt", O_RDWR);
 	if(0 > fd)
 	{
 		printf("open test.txt fail\n");
 		return -1;
 	}
+	
 	flags = fcntl(fd, F_GETFL, 0);
+	accmode = flags & O_ACCMODE;
+	if(accmode == O_RDONLY)
+	{
+		printf("STDIN READONLY\n");
+	}
+	else if(accmode == O_WRONLY)
+	{
+		printf("STDIN WRITEONLY\n");
+	}
+	else if(accmode == O_RDWR)
+	{
+		printf("STDIN RDWR\n");
+	}
+	else
+	{
+		printf("STDIN UNKNOWN MODE\n");
+	}
+
+	if(flags & O_APPEND)
+	{
+		printf("STDIN APPEND\n");
+	}
+
+	if(flags & O_NONBLOCK)
+	{
+		printf("STDIN NONBLOCK\n");
+	}
+
 
 	flags |= O_APPEND;
-	flags = fcntl(fd, F_SETFL, &flags);
+
+	accmode = flags & O_ACCMODE;
+	if(accmode == O_RDONLY)
+	{
+		printf("STDIN READONLY\n");
+	}
+	else if(accmode == O_WRONLY)
+	{
+		printf("STDIN WRITEONLY\n");
+	}
+	else if(accmode == O_RDWR)
+	{
+		printf("STDIN RDWR\n");
+	}
+	else
+	{
+		printf("STDIN UNKNOWN MODE\n");
+	}
+
+	if(flags & O_APPEND)
+	{
+		printf("STDIN APPEND\n");
+	}
+
+	if(flags & O_NONBLOCK)
+	{
+		printf("STDIN NONBLOCK\n");
+	}
+
+	/*书中的代码是错误的，当设置结构体的时候需要添加结构指针，标志位的时候直接使用long int*/
+	flags = fcntl(fd, F_SETFL, flags);
 	if(0 > flags)
 	{
 		printf("fail to set\n");
 		close(fd);
 		return -1;
 	}
+
 
 	write(fd, buf, strlen(buf));
 
