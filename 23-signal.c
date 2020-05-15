884c81ed9049913ce73bdc6c101a1b8b997a6565
crazyGo-Li <48678562+crazyGo-Li@users.noreply.github.com>
2020-05-15 13:44:01 +0800

    Update 23-signal.c

--- 23-signal.c
+++ 23-signal.c
@@ -4,10 +4,13 @@
 #include <string.h>
 #include <errno.h>
 
-#if 0
+/*SIGSTOP 和 SIGKILL 信号无法被捕获或忽略，signal会出错*/
+
+#if 1
 typedef void (*sighandler_t)(int);
 void sig_handle(int signo)
 {
+	/*
 	if(SIGSTOP == signo)
 	{
 		printf("get signal SIGSTOP\n");
@@ -20,23 +23,25 @@ void sig_handle(int signo)
 	{
 		printf("get signal %s\n", signo);
 	}
+	*/
+	printf("%d\n", signo);
 	return;
 }
 
 
 int main(void)
 {
-	sighandler_t ret;
-	ret = signal(SIGSTOP, sig_handle);
+	sighandler_t ret, func=sig_handle;
+	ret = signal(SIGINT, func);
 	if(SIG_ERR == ret)
 	{
-		printf("link SIGSTOP fail %s\n", strerror(errno));
+		printf("link SIGINT fail %s\n", strerror(errno));
 		return -1;
 	}
-	ret = signal(SIGKILL, sig_handle);
+	ret = signal(SIGTERM, sig_handle);
 	if(SIG_ERR == ret)
 	{
-		printf("link SIGKILL fail %s\n", strerror(errno));
+		printf("link SIGTERM fail %s\n", strerror(errno));
 		return -1;
 	}
 
@@ -46,6 +51,7 @@ int main(void)
 }
 #endif
 
+#if 0
 typedef void (* signal_handler)(int);
 
 void signal_handler_fun(int signal_num)
@@ -55,9 +61,15 @@ void signal_handler_fun(int signal_num)
 
 int main(void)
 {
-	signal_handler p_signal = signal_handler_fun;
-	signal(SIGINT, p_signal);
+	signal_handler p_signal = signal_handler_fun, ret;
+	ret = signal(SIGKILL, p_signal);
+	if(ret = SIG_ERR)
+	{
+		printf("error %s\n", strerror(errno));
+		return -1;
+	}
 	for(;;);
 	return 0;
 }
+#endif
 
