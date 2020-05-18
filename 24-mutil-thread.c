8158b6b6bc862e55c2cf6abc83f7aa5539733892
crazyGo-Li <48678562+crazyGo-Li@users.noreply.github.com>
2020-05-18 22:00:32 +0800

    add mutex and attr set

--- 24-mutil-thread.c
+++ 24-mutil-thread.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sched.h>
 
 static int run = 1;
 static int retvalue;
@@ -24,10 +25,24 @@ void *static_routine(void *arg)
 int main(void)
 {
 	pthread_t pt;
+	pthread_attr_t attr;
+	struct sched_param sch;
 	int ret = -1;
 	int times = 3;
 	int i = 0;
 	int *ret_join = NULL;
+
+	/*set thread attr*/
+	pthread_attr_getschedparam(&attr, &sch);
+
+	sch.sched_priority = 256;
+
+	pthread_attr_setschedparam(&attr, &sch);
+
+	/* set scope, PTHREAD_SCOPE_PROCESS/PTHREAD_SCOPE_SYSTEM*/
+	pthread_attr_init(&attr);
+	pthread_attr_setscope(&attr, PTHREAD_SCOPE_PROCESS);
+	
 	ret = pthread_create(&pt, NULL, (void *)static_routine, &run);
 	if(ret != 0)
 	{
